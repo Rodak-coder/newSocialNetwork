95afb53df4af4c3ea474ab5d30c5a180bbfbc15a
Rodak-coder <allcity8888@gmail.com>
2021-11-06 01:27:35 +0300

    Test commit code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {findAllByDisplayValue} from "@testing-library/react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      Chomp on this
+          <Rating />
+          <Accordion />
+      </div>
   );
+
+  function Rating() {
+    return (
+        <Star />
+    )
+  };
+
+  function Star() {
+      return (
+          <div>
+              <div>star</div>
+              <div>star</div>
+              <div>star</div>
+              <div>star</div>
+          </div>
+      )
+  }
+  function Accordion() {
+      return <div>
+          <h3>Menu</h3>
+          <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+          </ul>
+
+      </div>
+  }
 }
 
 export default App;
