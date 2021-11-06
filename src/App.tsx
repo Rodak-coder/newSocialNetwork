1db928739854ef4847ba0e256421641e572bb76f
Rodak-coder <allcity8888@gmail.com>
2021-11-06 12:05:05 +0300

    Test commit code

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {findAllByDisplayValue} from "@testing-library/react";
+import Accordion from "./components/Accordion/Accordion";
+import Rating from "./components/Rating/Rating";
 
 
 function App() {
-  return (
-      <div>
-          <AppTitle />
-          <Rating />
-          <Accordion />
-      </div>
-  );
-
+    return (
+        <div>
+            <input/>
+            <input value={'yoyoyo'} type="date"/>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+        </div>
+    )
+}
   function AppTitle(){
       return <>Chomp on this</>
-  };
-
-  function Rating() {
-    return <>
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-    </>
-  };
-
-  function Star() {
-      return  <div>star</div>
-  };
-
-  function Accordion() {
-      return <div>
-          <AccordionTittle />
-          <AccordionBody />
-      </div>
-  };
-
-  function AccordionTittle() {
-      return <h3>Menu</h3>
-  };
-
-  function AccordionBody() {
-      return <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-      </ul>
   }
 
-}
 
 export default App;
+
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Rating() {
+    return <div>
+        <Star selected={true}/>
+        <Star selected={true}/>
+        <Star selected={false}/>
+        <Star selected={false}/>
+        <Star selected={false}/>
+    </div>
+}
+
+function Star(props: any) {
+    return  <span><b>star</b></span>
+}
+
+export default  Rating;
